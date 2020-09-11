b50640aa10e2d5016f53f943bc7b19ce44bcc9c1
trevorism <trevorism@gmail.com>
2020-09-10 22:29:13 -0400

    adding user and app registration

--- src/main/groovy/com/trevorism/gcloud/webapi/controller/AppController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/controller/AppController.groovy
@@ -1,4 +1,57 @@
 package com.trevorism.gcloud.webapi.controller
 
+import com.trevorism.gcloud.webapi.model.App
+import com.trevorism.gcloud.webapi.model.Identity
+import com.trevorism.gcloud.webapi.model.User
+import com.trevorism.gcloud.webapi.service.AppRegistrationService
+import com.trevorism.gcloud.webapi.service.DefaultAppRegistrationService
+import io.swagger.annotations.Api
+
+import javax.ws.rs.*
+import javax.ws.rs.core.MediaType
+
+@Api("App Operations")
+@Path("/app")
 class AppController {
+
+    AppRegistrationService appRegistrationService = new DefaultAppRegistrationService()
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    App registerApp(App app) {
+        appRegistrationService.registerApp(app)
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<App> listUsers() {
+        appRegistrationService.listRegisteredApps()
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{clientId}")
+    App getAppByClientId(@PathParam("clientId") String clientId) {
+        appRegistrationService.getIdentity(clientId)
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{clientId}")
+    App removeUser(@PathParam("clientId") String clientId) {
+        App app = appRegistrationService.getIdentity(clientId)
+        appRegistrationService.removeRegisteredApp(app.id)
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{clientId}/secret")
+    String updateAppSecret(@PathParam("clientId") String clientId) {
+        App app = appRegistrationService.getIdentity(clientId)
+        appRegistrationService.generateClientSecret(app)
+    }
 }
--- src/main/groovy/com/trevorism/gcloud/webapi/controller/UserController.groovy
+++ src/main/groovy/com/trevorism/gcloud/webapi/controller/UserController.groovy
@@ -1,4 +1,71 @@
 package com.trevorism.gcloud.webapi.controller
 
+import com.trevorism.gcloud.webapi.model.Identity
+import com.trevorism.gcloud.webapi.model.User
+import com.trevorism.gcloud.webapi.service.DefaultUserCredentialService
+import com.trevorism.gcloud.webapi.service.UserCredentialService
+import io.swagger.annotations.Api
+
+import javax.ws.rs.Consumes
+import javax.ws.rs.DELETE
+import javax.ws.rs.GET
+import javax.ws.rs.POST
+import javax.ws.rs.Path
+import javax.ws.rs.PathParam
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Api("User Operations")
+@Path("/user")
 class UserController {
+
+    UserCredentialService userCredentialService = new DefaultUserCredentialService()
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    User registerUser(User user) {
+        userCredentialService.registerUser(user)
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    List<User> listUsers() {
+        userCredentialService.listUsers()
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{username}")
+    Identity getUser(@PathParam("username") String username) {
+        userCredentialService.getIdentity(username)
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{username}")
+    User removeUser(@PathParam("username") String username) {
+        User user = userCredentialService.getIdentity(username)
+        userCredentialService.deleteUser(user.id)
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{username}/activate")
+    boolean activateUser(@PathParam("username") String username) {
+        User user = userCredentialService.getIdentity(username)
+        userCredentialService.activateUser(user)
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("{username}/deactivate")
+    boolean deactivateUser(@PathParam("username") String username) {
+        User user = userCredentialService.getIdentity(username)
+        userCredentialService.deactivateUser(user)
+    }
 }
