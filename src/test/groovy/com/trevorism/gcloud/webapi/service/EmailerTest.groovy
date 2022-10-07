4cf0e0619e0ee8750dd00bad1290bad4c2d394a3
Trevor Brooks <trevorism@gmail.com>
2022-10-06 23:08:22 -0400

    Fix unit test based on dependency update

--- src/test/groovy/com/trevorism/gcloud/webapi/service/EmailerTest.groovy
+++ src/test/groovy/com/trevorism/gcloud/webapi/service/EmailerTest.groovy
@@ -1,13 +1,14 @@
 package com.trevorism.gcloud.webapi.service
 
 import com.trevorism.https.SecureHttpClient
+import com.trevorism.model.Email
 import org.junit.Test
 
 class EmailerTest {
 
     @Test
     void testSendForgotPasswordEmail() {
-        Emailer emailer = new Emailer([post: { x, y, z -> "true" }] as SecureHttpClient)
+        Emailer emailer = new Emailer([post: { x, y, z -> "{}" }] as SecureHttpClient)
         assert emailer.sendForgotPasswordEmail("trevorism@gmail.com", "12345678")
     }
 }
